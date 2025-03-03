44f6b9e8a99ff45e1e428e33f7f8cebd4416fa42
SylvanFranklin <sylvanfranklin@icloud.com>
2025-03-03 13:12:42 -0500

    feat: rename to __init__

---
+++ src/stormy/__init__.py
@@ -0,0 +1,94 @@
+import argparse
+import stormy.gifts as gifts
+import stormy.voyage as voyage
+import stormy.tokens as tokens
+import stormy.themes as themes
+import stormy.hospitality as hospitality
+from stormy.page import layout_pages
+
+
+def download_sheets():
+    import stormy.utils as utils
+
+    for i in ["voyage", "hospitality", "gifts", "themes", "new themes"]:
+        utils.download_csv_file(i)
+
+
+def compile_files(args):
+    print("Compiling files...")
+
+    if not args or "all" in args:
+        gifts.compile_all()
+        themes.compile_all()
+        hospitality.compile_all()
+        voyage.compile_all()
+        tokens.compile_all()
+        return
+
+    if "gifts" in args:
+        gifts.compile_all()
+    if "themes" in args:
+        themes.compile_all()
+    if "hospitality" in args:
+        hospitality.compile_all()
+    if "voyage" in args:
+        voyage.compile_all()
+    if "tokens" in args:
+        tokens.compile_all()
+
+
+def generate_pages(args=None):
+    if not args or "all" in args:
+        layout_pages("voyage")
+        layout_pages("gifts")
+        layout_pages("themes")
+        layout_pages("hospitality")
+        return
+
+    if "voyage" in args:
+        layout_pages("voyage")
+    if "gifts" in args:
+        layout_pages("gifts")
+    if "themes" in args:
+        layout_pages("themes")
+    if "hospitality" in args:
+        layout_pages("hospitality")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Stormy CLI tool")
+    subparsers = parser.add_subparsers(dest="command", help="Command to run")
+    compile_parser = subparsers.add_parser("compile", help="Compile files")
+    compile_parser.add_argument(
+        "types",
+        nargs="*",
+        choices=["gifts", "themes", "hospitality", "voyage", "tokens", "all"],
+        default=["all"],
+        help="Types to compile",
+    )
+
+    pages_parser = subparsers.add_parser("pages", help="Generate pages")
+    pages_parser.add_argument(
+        "types",
+        nargs="*",
+        choices=["voyage", "gifts", "themes", "hospitality", "all"],
+        default=["all"],
+        help="Types of pages to generate",
+    )
+
+    return parser.parse_args()
+
+
+def cli():
+    args = parse_args()
+
+    if args.command == "dl":
+        download_sheets()
+    elif args.command == "compile":
+        compile_files(args.types)
+    elif args.command == "pages":
+        generate_pages(args.types)
+
+
+if __name__ == "__main__":
+    cli()
